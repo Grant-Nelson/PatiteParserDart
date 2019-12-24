7345d6fe1305c7e80908ed4a3ba635d49df03b9c
Grant Nelson <grantnelson1@gmail.com>
2019-12-24 15:39:00 -0700

    Fixed the test tools

--- test/testArgs.dart
+++ test/testArgs.dart
@@ -1,25 +1,33 @@
 part of PatiteParserDart.test;
 
+/// The arguments passed into a test.
 class TestArgs {
   bool _failed;
   StringBuffer _buf;
 
+  /// Creates a new test argument.
   TestArgs() {
     this._failed = false;
     this._buf = new StringBuffer();
   }
 
+  /// Indicates if the test has failed or not.
   bool get failed => this._failed;
+
+  /// Gets the buffered log for this test.
   String toString() => this._buf.toString();
 
+  /// Writes an output to the test log.
   void log(String msg) =>
     this._buf.writeln(msg);
 
+  /// Indicates an error occurred.
   void error(String msg) {
     this._buf.writeln('Error: '+msg);
     this._failed = true;
   }
 
+  /// Checks the tokenizer results.
   void checkTok(Tokenizer tok, String input, List<String> expected) {
     StringBuffer resultBuf = new StringBuffer();
     for (Token token in tok.tokenize(input))
@@ -35,6 +43,7 @@ class TestArgs {
     }
   }
   
+  /// Checks the parser will parse the given input.
   void checkParser(Parser parser, List<String> input, List<String> expected) {
     Result parseResult = parser.parse(input.join('\n'));
     String exp = expected.join('\n');
--- test/testTool.dart
+++ test/testTool.dart
@@ -1,11 +1,19 @@
 part of PatiteParserDart.test;
 
+/// The main tool for testing.
 class TestTool {
   bool _failed;
 
+  /// Creates a new testing tool.
+  TestTool() {
+    this._failed = false;
+  }
+
+  /// prints the results of all the tests.
   void printResult() =>
     print(this._failed? 'FAILED': 'PASSED');
 
+  /// Runs a test given the test function.
   void run(Function(TestArgs args) test) {
     TestArgs args = new TestArgs();
     test(args);
